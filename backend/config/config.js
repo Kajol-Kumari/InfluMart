39393a000573eb2bb8d2de6974063dc410666520
Apple <apple@Apples-MacBook-Pro.local>
2023-10-07 16:13:03 +0530

    modified db code as per new standard

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
   MONGO_DB_URL: process.env.MONGO_DB_URL,
+  DB_NAME: process.env.DB_NAME,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
   BASE_URL: process.env.BASE_URL,
--- backend/middleware/db.js
+++ backend/middleware/db.js
@@ -1,38 +1,30 @@
-const mongoose = require("mongoose");
-const config = require("../config/config");
+const mongoose = require('mongoose');
+const config = require('../config/config');
 
-function connect() {
-  mongoose.connect(config.MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "playground",
-  });
-  mongoose.Promise = Promise;
-
-  // When successfully connected
-  mongoose.connection.on("connected", () => {
-    console.log(`Mongoose default connection open`);
-  });
-
-  // If the connection throws an error
-  mongoose.connection.on("error", (err) => {
-    console.log(`Mongoose default connection error: ${err}`);
-  });
-
-  // When the connection is disconnected
-  mongoose.connection.on("disconnected", () => {
-    console.log("Mongoose default connection disconnected");
-  });
-
-  // If the Node process ends, close the Mongoose and Redis connection
-  process.on("SIGINT", () => {
-    mongoose.connection.close(() => {
-      console.log(
-        "Mongoose default connection disconnected through app termination"
-      );
-      process.exit();
+async function connect() {
+  try {
+    await mongoose.connect(config.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: config.DB_NAME,
     });
-  });
+    console.log('Mongoose default connection open');
+
+    // Handle connection close on process exit
+    process.on('SIGINT', async () => {
+      try {
+        await mongoose.connection.close();
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+      } catch (err) {
+        console.error('Error closing Mongoose connection:', err);
+        process.exit(1);
+      }
+    });
+  } catch (err) {
+    console.error('Mongoose default connection error:', err);
+    process.exit(1);
+  }
 }
 
-module.exports = connect();
+module.exports = connect;
