751df03c3f9d8c6d423b0d6b1fe76ce84193cccb
Hemu21 <rajva5727@gmail.com>
2024-06-06 21:55:05 +0530

    fix

--- backend/model/Subscription.js
+++ backend/model/Subscription.js
@@ -5,6 +5,10 @@ const subscriptionSchema = new mongoose.Schema({
   plan: { type: String, required: true },
   startDate: { type: Date, default: Date.now },
   endDate: { type: Date, required: true },
+  isFree: { type: Boolean, required: true }, // Flag to denote if it's a free subscription
+  amount: { type: Number }, // Payment amount, applicable if it's a charged subscription
+  paymentMode: { type: String }, // Mode of payment (e.g., credit card, PayPal)
+  transactionDate: { type: Date },
 });
 
 const Subscription = mongoose.model('Subscription', subscriptionSchema);
