4375095319a2aa45e6204146574a7a1c00aa16d7
Kajol Kumari <kajolkumarisingh222@gmail.com>
2024-06-08 14:12:39 +0530

    🍀 added backend url

--- backend/README.md
+++ backend/README.md
@@ -11,3 +11,5 @@ Run the following commands;
 - npm start
 
 Your backend server will start running at http://localhost:3000
+
+NOTE - The backend server is live at [https://influmart.onrender.com/](https://influmart.onrender.com/)
---
+++ backend/model/brandDbRequestModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const brandSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "brand", // Set the default value to "brand"
+  },
+});
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+module.exports = Brand;
