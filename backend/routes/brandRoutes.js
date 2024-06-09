946b533cc489eab19384d714a1f7ac3e8cbcb721
Hemu21 <rajva5727@gmail.com>
2024-06-09 11:11:48 +0530

    auth

--- backend/routes/brandRoutes.js
+++ backend/routes/brandRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { signup, login, getProfile, updateProfile } = require('../controllers/brandController');
 const brandAuthenticationMiddleware = require('../middleware/brands/brandAuthenticationMiddleware');
 const brandSignUpValidate = require('../middleware/brands/validations/brandSignUpValidate');
+const brandAuthorizationMiddleware = require('../middleware/brands/brandAuthorizationMiddleware');
 
 // Signup a brand
 router.post('/signup',brandSignUpValidate, signup);
@@ -11,9 +12,9 @@ router.post('/signup',brandSignUpValidate, signup);
 router.post('/login', login);
 
 // Get brand's profile
-router.get('/profile',brandAuthenticationMiddleware, getProfile);
+router.get('/profile',brandAuthenticationMiddleware,brandAuthorizationMiddleware, getProfile);
 
 // Update brand's profile
-router.put('/profile',brandAuthenticationMiddleware, updateProfile);
+router.put('/profile',brandAuthenticationMiddleware,brandAuthorizationMiddleware, updateProfile);
 
 module.exports = router;
