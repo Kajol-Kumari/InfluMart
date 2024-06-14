c44672470f78b17accfae56c7bfef8319a93c9f2
Balaharisankar Lakshmanaperumal <114602603+BHS-Harish@users.noreply.github.com>
2024-06-14 11:51:46 +0530

    🍀 Login pages integrated with backend (#33)
    * Login pages integrated with backend
    
    * Password field fixed
    
    * refactored the files

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,16 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin',
-      ["module:react-native-dotenv", {
-        "envName": "APP_ENV",
-        "moduleName": "@env",
-        "path": ".env",
-        "safe": false,
-        "allowUndefined": true,
-        "verbose": false
-      }]
+    plugins: ['react-native-reanimated/plugin',["module:react-native-dotenv", {
+      "envName": "APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+      "safe": false,
+      "allowUndefined": false,
+      "verbose": false
+    }]
     ]
-    
   };
 };
