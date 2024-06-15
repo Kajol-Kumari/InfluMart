26fd6b18e61a78e57cb0b7971d2c18cef256978f
Vidip Ghosh <vidip2001@gmail.com>
2024-06-15 15:16:51 +0530

    [Frontend]: fixes responsiveness of brand login page

--- frontend/screens/signup/SignUpStyles.scss.js
+++ frontend/screens/signup/SignUpStyles.scss.js
@@ -1,255 +1,265 @@
-import { Color, Padding, FontSize, Border, FontFamily } from "../../GlobalStyles";
+import {
+  Color,
+  Padding,
+  FontSize,
+  Border,
+  FontFamily,
+} from "../../GlobalStyles";
 
 export const signupStyles = {
-    scrollView: {
-      flex: 1,
-    },
-    frameLayout2: {
-      width: 390,
-      backgroundColor: Color.colorWhitesmoke_100,
-    },
-    depth1FrameSpaceBlock: {
-      paddingHorizontal: Padding.p_base,
-      width: 390,
-    },
-    frameFlexBox: {
-      justifyContent: "space-between",
-      height: 48,
-      alignItems: "center",
-      flexDirection: "row",
-    },
-    frameLayout1: {
-      width: 48,
-      height: 48,
-    },
-    frameLayout: {
-      height: 88,
-      width: 358,
-    },
-    emailTypo: {
-      textAlign: "left",
-      lineHeight: 24,
-      fontSize: FontSize.size_base,
-    },
-    depth5FrameBg: {
-      backgroundColor: Color.colorAliceblue,
-      overflow: "hidden",
-    },
-    depth4FrameLayout: {
-      paddingVertical: 0,
-      height: 40,
-      justifyContent: "center",
-      borderRadius: Border.br_xs,
-      alignItems: "center",
-      flexDirection: "row",
-      width: 358,
-      paddingHorizontal: Padding.p_base,
-    },
-    signUpTypo: {
-      lineHeight: 21,
-      fontSize: FontSize.size_sm,
-      textAlign: "left",
-      fontFamily: FontFamily.workSansBold,
-      fontWeight: "700",
-      letterSpacing: 0,
-    },
-    depth3FrameLayout: {
-      height: 40,
-      justifyContent: "center",
-      flexDirection: "row",
-      width: 358,
-    },
-    loginFrame: {
-      height: 40,
-      justifyContent: "center",
-      flexDirection: "row",
-      width: 358,
-      alignItems: "center",
-    },
-    frameBg: {
-      backgroundColor: Color.colorDodgerblue,
-      overflow: "hidden",
-    },
-    depth4Frame0: {
-      width: 24,
-      height: 24,
-    },
-    depth3Frame0: {
-      alignItems: "center",
-      flexDirection: "row",
-    },
-    depth5Frame0: {
-      justifyContent: "flex-end",
-      alignItems: "center",
-    },
-    depth4Frame01: {
-      flexDirection: "row",
-    },
-    depth3Frame1: {
-      width: 310,
-      paddingLeft: Padding.p_243xl,
-    },
-    depth2Frame0: {
-      width: 358,
-    },
-    depth1Frame0: {
-      height: 72,
-      paddingTop: Padding.p_base,
-      paddingBottom: Padding.p_5xs,
-      backgroundColor: Color.colorWhitesmoke_100,
-      paddingHorizontal: Padding.p_base,
-    },
-    createAnAccount: {
-      fontSize: FontSize.size_3xl,
-      lineHeight: 28,
-      textAlign: "center",
-      color: Color.colorBlack,
-      fontFamily: FontFamily.workSansBold,
-      fontWeight: "700",
-      letterSpacing: 0,
-    },
-    depth2Frame01: {
-      alignSelf: "stretch",
-      alignItems: "center",
-    },
-    depth1Frame1: {
-      height: 60,
-      paddingTop: Padding.p_xl,
-      paddingBottom: Padding.p_xs,
-      alignItems: "center",
-    },
-    email: {
-      fontWeight: "500",
-      fontFamily: FontFamily.workSansMedium,
-      color: Color.colorBlack,
-      alignSelf: "stretch",
-    },
-    depth4Frame02: {
-      height: 32,
-      width: 358,
-      paddingBottom: Padding.p_5xs,
-    },
-    email1: {
-      fontFamily: FontFamily.workSansRegular,
-      color: Color.colorSteelblue_200,
-    },
-    depth7Frame0: {
-      alignSelf: "stretch",
-    },
-    depth6Frame0: {
-      width: "auto",
-      height: 24,
-      overflow: "hidden",
-    },
-    depth5Frame01: {
-      borderRadius: Border.br_xs,
-      backgroundColor: Color.colorAliceblue,
-      height: 56,
-      justifyContent: "space-between",
-      flexDirection: "row",
-      width: 358,
-      zIndex: 1,
-    },
-    depth4Frame1: {
-      height: 56,
-      alignItems: "center",
-      flexDirection: "row",
-      width: 358,
-    },
-    depth2Frame02: {
-      alignItems: "flex-end",
-      flexDirection: "row",
-      zIndex: 1,
-    },
-    depth1Frame2: {
-      height: 112,
-      paddingVertical: Padding.p_xs,
-      alignItems: "center",
-      flexDirection: "row",
-      paddingHorizontal: Padding.p_base,
-      width: 390,
-      zIndex: 1,
-    },
-    depth6Frame01: {
-      width: "auto",
-      height: 24,
-      overflow: "hidden",
-    },
-    depth6Frame02: {
-      width: "auto",
-      height: 24,
-      overflow: "hidden",
-    },
-    depth6Frame03: {
-      width: "auto",
-      height: 24,
-      overflow: "hidden",
-    },
-    generateOtp: {
-      color: Color.colorGray_400,
-    },
-    depth5Frame05: {
-      width: "auto",
-      height: 21,
-    },
-    depth4Frame06: {
-      backgroundColor: Color.colorAliceblue,
-      overflow: "hidden",
-      borderRadius: Border.br_xs,
-    },
-    signUp: {
-      color: Color.colorWhitesmoke_400,
-    },
-    depth5Frame06: {
-      width: "auto",
-      height: 21,
-    },
-    depth4Frame07: {
-      paddingVertical: 0,
-      height: 40,
-      justifyContent: "center",
-      borderRadius: Border.br_xs,
-      alignItems: "center",
-      flexDirection: "row",
-      width: 358,
-      paddingHorizontal: Padding.p_base,
-    },
-    depth3Frame11: {
-      marginTop: 12,
-    },
-    depth2Frame06: {
-      height: 92,
-      alignItems: "center",
-      width: 358,
-    },
-    depth1Frame6: {
-      height: 116,
-      justifyContent: "center",
-      paddingVertical: Padding.p_xs,
-      flexDirection: "row",
-      paddingHorizontal: Padding.p_base,
-      width: 390,
-    },
-    depth1Frame7: {
-      height: 20,
-    },
-    depth0Frame0: {
-      overflow: "hidden",
-      height: 844,
-    },
-    brandregistrationform: {
-      backgroundColor: Color.colorWhite,
-      flex: 1,
-      width: "100%",
-      height: 844,
-    },
-    textInput: {
-      borderColor: Color.colorGray_400,
-      borderWidth: 0,
-      borderRadius: Border.br_xs,
-      paddingHorizontal: Padding.p_base,
-      fontSize: FontSize.size_base,
-      flex: 1,
-      color: Color.colorSteelblue_200,
-    },
-  }
+  scrollView: {
+    flex: 1,
+  },
+  frameLayout2: {
+    width: "100%",
+    backgroundColor: Color.colorWhitesmoke_100,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  depth1FrameSpaceBlock: {
+    paddingHorizontal: Padding.p_base,
+    width: "100%",
+  },
+  frameFlexBox: {
+    justifyContent: "space-between",
+    height: 48,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  frameLayout1: {
+    width: 48,
+    height: 48,
+  },
+  frameLayout: {
+    height: 88,
+    width: 358,
+  },
+  emailTypo: {
+    textAlign: "left",
+    lineHeight: 24,
+    fontSize: FontSize.size_base,
+  },
+  depth5FrameBg: {
+    backgroundColor: Color.colorAliceblue,
+    overflow: "hidden",
+  },
+  depth4FrameLayout: {
+    paddingVertical: 0,
+    height: 40,
+    justifyContent: "center",
+    borderRadius: Border.br_xs,
+    alignItems: "center",
+    flexDirection: "row",
+    width: 358,
+    paddingHorizontal: Padding.p_base,
+  },
+  signUpTypo: {
+    lineHeight: 21,
+    fontSize: FontSize.size_sm,
+    textAlign: "left",
+    fontFamily: FontFamily.workSansBold,
+    fontWeight: "700",
+    letterSpacing: 0,
+  },
+  depth3FrameLayout: {
+    height: 40,
+    justifyContent: "center",
+    flexDirection: "row",
+    width: 358,
+  },
+  loginFrame: {
+    height: 40,
+    justifyContent: "center",
+    flexDirection: "row",
+    width: 358,
+    alignItems: "center",
+  },
+  frameBg: {
+    backgroundColor: Color.colorDodgerblue,
+    overflow: "hidden",
+  },
+  depth4Frame0: {
+    width: 24,
+    height: 24,
+  },
+  depth3Frame0: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  depth5Frame0: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  depth4Frame01: {
+    flexDirection: "row",
+  },
+  depth3Frame1: {
+    width: 310,
+    paddingLeft: Padding.p_243xl,
+  },
+  depth2Frame0: {
+    width: "100%",
+  },
+  depth1Frame0: {
+    height: 72,
+    paddingTop: Padding.p_base,
+    paddingBottom: Padding.p_5xs,
+    backgroundColor: Color.colorWhitesmoke_100,
+    paddingHorizontal: Padding.p_base,
+    width: "100%",
+  },
+  createAnAccount: {
+    fontSize: FontSize.size_3xl,
+    lineHeight: 28,
+    textAlign: "center",
+    color: Color.colorBlack,
+    fontFamily: FontFamily.workSansBold,
+    fontWeight: "700",
+    letterSpacing: 0,
+  },
+  depth2Frame01: {
+    alignSelf: "stretch",
+    alignItems: "center",
+  },
+  depth1Frame1: {
+    height: 60,
+    paddingTop: Padding.p_xl,
+    paddingBottom: Padding.p_xs,
+    alignItems: "center",
+  },
+  email: {
+    fontWeight: "500",
+    fontFamily: FontFamily.workSansMedium,
+    color: Color.colorBlack,
+    alignSelf: "stretch",
+  },
+  depth4Frame02: {
+    height: 32,
+    width: 358,
+    paddingBottom: Padding.p_5xs,
+  },
+  email1: {
+    fontFamily: FontFamily.workSansRegular,
+    color: Color.colorSteelblue_200,
+  },
+  depth7Frame0: {
+    alignSelf: "stretch",
+  },
+  depth6Frame0: {
+    width: "auto",
+    height: 24,
+    overflow: "hidden",
+  },
+  depth5Frame01: {
+    borderRadius: Border.br_xs,
+    backgroundColor: Color.colorAliceblue,
+    height: 56,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    width: 358,
+    zIndex: 1,
+  },
+  depth4Frame1: {
+    height: 56,
+    alignItems: "center",
+    flexDirection: "row",
+    width: 358,
+  },
+  depth2Frame02: {
+    alignItems: "flex-end",
+    flexDirection: "row",
+    zIndex: 1,
+  },
+  depth1Frame2: {
+    height: 112,
+    paddingVertical: Padding.p_xs,
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: Padding.p_base,
+    width: 390,
+    zIndex: 1,
+  },
+  depth6Frame01: {
+    width: "auto",
+    height: 24,
+    overflow: "hidden",
+  },
+  depth6Frame02: {
+    width: "auto",
+    height: 24,
+    overflow: "hidden",
+  },
+  depth6Frame03: {
+    width: "auto",
+    height: 24,
+    overflow: "hidden",
+  },
+  generateOtp: {
+    color: Color.colorGray_400,
+  },
+  depth5Frame05: {
+    width: "auto",
+    height: 21,
+  },
+  depth4Frame06: {
+    backgroundColor: Color.colorAliceblue,
+    overflow: "hidden",
+    borderRadius: Border.br_xs,
+  },
+  signUp: {
+    color: Color.colorWhitesmoke_400,
+  },
+  depth5Frame06: {
+    width: "auto",
+    height: 21,
+  },
+  depth4Frame07: {
+    paddingVertical: 0,
+    height: 40,
+    justifyContent: "center",
+    borderRadius: Border.br_xs,
+    alignItems: "center",
+    flexDirection: "row",
+    width: 358,
+    paddingHorizontal: Padding.p_base,
+  },
+  depth3Frame11: {
+    marginTop: 12,
+  },
+  depth2Frame06: {
+    height: 92,
+    alignItems: "center",
+    width: 358,
+  },
+  depth1Frame6: {
+    height: 116,
+    justifyContent: "center",
+    paddingVertical: Padding.p_xs,
+    flexDirection: "row",
+    paddingHorizontal: Padding.p_base,
+    width: 390,
+  },
+  depth1Frame7: {
+    height: 20,
+  },
+  depth0Frame0: {
+    overflow: "hidden",
+    height: 844,
+  },
+  brandregistrationform: {
+    backgroundColor: Color.colorWhite,
+    flex: 1,
+    width: "100%",
+    height: 844,
+  },
+  textInput: {
+    borderColor: Color.colorGray_400,
+    borderWidth: 0,
+    borderRadius: Border.br_xs,
+    paddingHorizontal: Padding.p_base,
+    fontSize: FontSize.size_base,
+    flex: 1,
+    color: Color.colorSteelblue_200,
+  },
+};
