37c7826776c1f78acd4bc95c91f46295ed69136b
Siddheya Kulkarni <siddheyakul@gmail.com>
2024-06-18 13:36:22 +0530

    added auth middleware for collaborations

--- backend/routes/collaborationRoutes.js
+++ backend/routes/collaborationRoutes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { createCollaboration, getBrandCollaborations, getBrandMinimumRequirements, getBrandCollaborationAnalytics } = require('../controllers/collaborationController');
+const { 
+    createCollaboration, 
+    getBrandCollaborations, 
+    getBrandMinimumRequirements, 
+    getBrandCollaborationAnalytics 
+} = require('../controllers/collaborationController');
+const collaborationAuthenticationMiddleware = require("../middleware/collaborations/collaborationAuthenticationMiddleware");
+const collaborationAuthorizationMiddleware = require("../middleware/collaborations/collaborationAuthorizationMiddleware");
 
-router.post('/collaborations', createCollaboration);
+router.post('/collaborations', collaborationAuthenticationMiddleware,
+    collaborationAuthorizationMiddleware, createCollaboration);
 
-router.get('/collaborations/:brandId', getBrandCollaborations);
+router.get('/collaborations/:brandId', collaborationAuthenticationMiddleware,
+    collaborationAuthorizationMiddleware, getBrandCollaborations);
 
-router.get('/minimum-requirements/:brandId', getBrandMinimumRequirements);
+router.get('/minimum-requirements/:brandId', collaborationAuthenticationMiddleware,
+    collaborationAuthorizationMiddleware, getBrandMinimumRequirements);
 
-router.get('/analytics/:brandId', getBrandCollaborationAnalytics);
+router.get('/analytics/:brandId', collaborationAuthenticationMiddleware,
+    collaborationAuthorizationMiddleware, getBrandCollaborationAnalytics);
 
 module.exports = router;
