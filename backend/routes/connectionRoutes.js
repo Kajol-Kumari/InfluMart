bc9532d7df486753c9b0f38753f837a5519d2d5b
Hemu21 <rajva5727@gmail.com>
2024-06-22 21:15:30 +0530

    update

--- backend/routes/connectionRoutes.js
+++ backend/routes/connectionRoutes.js
@@ -5,18 +5,20 @@ const {
   accept,
   reject,
 } = require("../controllers/connectController");
+const brandAuthenticationMiddleware = require("../middleware/brands/brandAuthenticationMiddleware");
+const influencerAuthenticationMiddleware = require("../middleware/influencers/influencerAuthenticationMiddleware");
 const router = express.Router();
 
 // Send a connection request endpoint
-router.post("/send-request", sendRequest);
+router.post("/send-request", brandAuthenticationMiddleware ,sendRequest);
 
 // View all requests for a user endpoint
-router.get("/requests/:userId", allRequests);
+router.get("/requests/:userId", influencerAuthenticationMiddleware ,allRequests);
 
 // Accept a connection request endpoint
-router.post("/accept-request", accept);
+router.post("/accept-request", influencerAuthenticationMiddleware ,accept);
 
 // Reject a connection request endpoint
-router.post("/reject-request", reject);
+router.post("/reject-request", influencerAuthenticationMiddleware ,reject);
 
 module.exports = router;
