8963609112a93aafed9b5d31e2e249db64b5a2d2
balaharisankar <balaharisankar2003@gmail.com>
2024-06-23 23:39:31 +0530

    HomePage.js made as responsive

--- frontend/components/Depth1Frame4.js
+++ frontend/components/Depth1Frame4.js
@@ -1,27 +1,42 @@
 import * as React from "react";
 import { Image } from "expo-image";
-import { StyleSheet, View, Text } from "react-native";
-import { FontSize, FontFamily, Color, Padding } from "../GlobalStyles";
+import { StyleSheet, View, Text, TouchableOpacity, TextInput } from "react-native";
+import { FontSize, FontFamily, Color, Padding,Border } from "../GlobalStyles";
 
-const Depth1Frame4 = () => {
+const Depth1Frame4 = ({ onChange }) => {
+  const [isSearchBarOpen, setIsSearchBarOpen] = React.useState(false)
+
+  const handleSearch = () => {
+    setIsSearchBarOpen(!isSearchBarOpen)
+  }
+
   return (
     <View style={styles.depth1Frame0}>
       <View style={[styles.depth2Frame0, styles.frameFlexBox]}>
-        <View style={[styles.depth3Frame0, styles.depth3FrameLayout]}>
-          <Image
-            style={styles.depth4Frame0}
-            contentFit="cover"
-            source={require("../assets/depth-4-frame-0.png")}
-          />
-        </View>
-        <View style={[styles.depth3Frame1, styles.frameFlexBox]}>
-          <View style={styles.depth4Frame01}>
-            <View style={styles.depth5Frame0}>
-              <Text style={styles.influmart}>Influmart</Text>
-            </View>
-          </View>
-        </View>
-        <View style={styles.depth3FrameLayout}>
+        {
+          isSearchBarOpen ?
+            <TextInput onChange={(e) => {
+              onChange(e.target.value)
+            }} style={styles.SearchBar} placeholder="Search anything" /> :
+            <>
+              <TouchableOpacity style={[styles.depth3Frame0, styles.depth3FrameLayout]}>
+                <Image
+                  style={styles.depth4Frame0}
+                  contentFit="cover"
+                  source={require("../assets/depth-4-frame-0.png")}
+                />
+              </TouchableOpacity>
+              <View style={[styles.depth3Frame1, styles.frameFlexBox]}>
+                <View style={styles.depth4Frame01}>
+                  <View style={styles.depth5Frame0}>
+                    <Text style={styles.influmart}>Influmart</Text>
+                  </View>
+                </View>
+              </View>
+            </>
+
+        }
+        <TouchableOpacity onPress={handleSearch} style={styles.depth3FrameLayout}>
           <View style={[styles.depth4Frame02, styles.depth3FrameLayout]}>
             <Image
               style={styles.depth4Frame0}
@@ -29,7 +44,7 @@ const Depth1Frame4 = () => {
               source={require("../assets/depth-5-frame-0.png")}
             />
           </View>
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -43,6 +58,8 @@ const styles = StyleSheet.create({
   depth3FrameLayout: {
     width: 48,
     flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     height: 48,
   },
   depth4Frame0: {
@@ -80,20 +97,30 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   depth2Frame0: {
-    width: 358,
+    width: "auto",
     justifyContent: "space-between",
     alignItems: "center",
-    height: 48,
+    height: "auto",
     flexDirection: "row",
   },
   depth1Frame0: {
     backgroundColor: Color.colorWhite,
-    width: 390,
+    width: "100%",
     height: 72,
     paddingHorizontal: Padding.p_base,
     paddingTop: Padding.p_base,
     paddingBottom: Padding.p_5xs,
   },
+  SearchBar: {
+    width: "80%",
+    paddingVertical: Padding.p_smi,
+    paddingHorizontal: Padding.p_base,
+    fontSize: FontSize.size_base,
+    color: Color.colorSteelblue_200,
+    backgroundColor: Color.colorAliceblue,
+    outlineStyle: "none",
+    borderRadius: Border.br_xs
+  }
 });
 
 export default Depth1Frame4;
