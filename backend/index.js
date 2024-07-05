bb1f0793842ec95457ec177c710b566c1dffc5ce
Hemanth kumar <rajva5727@gmail.com>
2024-07-05 14:58:20 +0530

    🍀 [frontend] After verification of Youtube account, it's not able to redirect to the app  Fixed (#187)
    * fix
    
    * update

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
   next();
 });
 // Verification routes
-app.use('/', authRoutes);
+app.use('/api', authRoutes);
 
 // Mount the payment controller routes
 app.use('/api/payment', paymentRoutes);
