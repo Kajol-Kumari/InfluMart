b7b14cfbacd649878d829f6513f50c3058844e7d
balaharisankar <balaharisankar2003@gmail.com>
2024-07-06 17:40:18 +0530

    InfluencerList UI Fixed

--- frontend/screens/InfluencerList/IncluencerCard.js
+++ frontend/screens/InfluencerList/IncluencerCard.js
@@ -12,6 +12,7 @@ import { useNavigation } from "@react-navigation/core";
 import ImageWithFallback from "../../util/ImageWithFallback";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useAlert } from "../../util/AlertContext";
+import { LinearGradient } from 'expo-linear-gradient'
 
 
 const InfluencerCard = ({
@@ -20,42 +21,42 @@ const InfluencerCard = ({
   beauty,
   influencerId,
 }) => {
-  const {showAlert} = useAlert()
+  const { showAlert } = useAlert()
   const navigation = useNavigation()
-  const [navigate,setnavigate] = React.useState(false)
-  React.useEffect(()=>{
-    const getData = async ()=>{
+  const [navigate, setnavigate] = React.useState(false)
+  React.useEffect(() => {
+    const getData = async () => {
       const brand = await AsyncStorage.getItem("brandId")
       const influencer = await AsyncStorage.getItem("influencerId")
-      if(!brand && !influencer){
+      if (!brand && !influencer) {
         setnavigate(false)
-      }else{
+      } else {
         setnavigate(true)
       }
     }
     getData()
   })
-  const handleClick = async ()=>{
-    if(navigate){
-      navigation.navigate("Analytics",{influencerId})
-    }else{
+  const handleClick = async () => {
+    if (navigate) {
+      navigation.navigate("Analytics", { influencerId })
+    } else {
       showAlert("Login Require!", "Please login to see Influencer's Analytics.")
     }
   }
   return (
     <View style={styles.cardContainer}>
       <View style={styles.cardContent}>
-        <TouchableOpacity onPress={()=>handleClick()}>
+        <TouchableOpacity onPress={() => handleClick()}>
           <View style={styles.imageContainer}>
             <ImageWithFallback image={depth5Frame0} imageStyle={styles.image} />
-          </View>
-          <View style={styles.textContainer}>
-            <View style={styles.titleContainer}>
-              <Text style={styles.title}>{kylieCosmetics}</Text>
+            <View style={styles.overlayContainer}>
+              <LinearGradient style={styles.overlay} colors={['transparent', '#000']}>
+                <Text style={styles.insightText}>INSIGHT</Text>
+                <Text style={styles.google}>{kylieCosmetics}</Text>
+                <Text style={styles.insightText}>Random Text</Text>
+              </LinearGradient>
             </View>
-            <View style={styles.categoryContainer}>
-              <Text style={styles.category}>{beauty}</Text>
-            </View>
+            <Text style={styles.categoryText}>{beauty}</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -65,11 +66,11 @@ const InfluencerCard = ({
 
 const styles = StyleSheet.create({
   cardContainer: {
-    height: 316,
-    width: 360,
+    height: 420,
+    width: 280,
     backgroundColor: Color.colorBlack,
-    padding: Padding.p_base,
     borderRadius: Border.br_xs,
+    margin: Padding.p_base
   },
   cardContent: {
     height: "100%",
@@ -77,14 +78,15 @@ const styles = StyleSheet.create({
     borderRadius: Border.br_xs,
   },
   imageContainer: {
-    height: 191,
-    width: 328,
+    height: 420,
+    width: 280,
     borderRadius: Border.br_xs,
     overflow: "hidden",
+    backgroundColor:"#0C0B0B"
   },
   image: {
-    height: "100%",
-    width: "100%",
+    height: 350,
+    width: 280,
     borderRadius: Border.br_xs,
   },
   textContainer: {
@@ -120,6 +122,49 @@ const styles = StyleSheet.create({
     color: "#ccc",
     textAlign: "left",
   },
+  overlayContainer: {
+    width: 280,
+    height: 350,
+    position: "absolute",
+    top: 0,
+    overflow:"hidden"
+  },
+  overlay: {
+    height: "100%",
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"flex-end",
+    padding:Padding.p_base,
+    gap:3
+  },
+  insightText: {
+    width:"100%",
+    fontSize: FontSize.size_xs,
+    fontFamily: FontFamily.plusJakartaSansBold,
+    color: Color.colorSlategray_100,
+  },
+  google: {
+    fontSize: FontSize.size_base,
+    lineHeight: 24,
+    fontFamily: FontFamily.interMedium,
+    fontWeight: "500",
+    textAlign: "left",
+    color: Color.colorWhite,
+    alignSelf: "stretch",
+  },
+  categoryText:{
+    width:"100%",
+    position:"absolute",
+    top:360,
+    fontSize: FontSize.size_base,
+    lineHeight: 24,
+    fontFamily: FontFamily.interMedium,
+    fontWeight: "500",
+    textAlign: "left",
+    color: "#ccc",
+    alignSelf: "stretch",
+    paddingHorizontal:Padding.p_base
+  }
 });
 
 export default InfluencerCard;
