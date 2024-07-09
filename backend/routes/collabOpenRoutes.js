8f552d5e2b79ef22c064a466c189b298a708dad3
Hemu21 <rajva5727@gmail.com>
2024-07-10 00:17:43 +0530

    Added

--- backend/routes/collabOpenRoutes.js
+++ backend/routes/collabOpenRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { postCollabOpen } = require('../controllers/CollabOpenController');
+const { postCollabOpen, getAllCollabOpen } = require('../controllers/CollabOpenController');
 const brandAuthenticationMiddleware = require('../middleware/brands/brandAuthenticationMiddleware');
 
 router.post('/collab-open',brandAuthenticationMiddleware, postCollabOpen);
 
-
+router.get('/get-collab-open',getAllCollabOpen)
 
 
 module.exports = router;
